ed5c97f71a00f5c8cfce3db5f78d9fc97cbc65d3
sanjay8330 <sanjayabi8330@gmail.com>
2021-05-05 07:44:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  /**The attributes of product form */
+  const [productID, setproductID] = useState(0);
+  const [productName, setproductName] = useState("");
+  const [productDescription, setproductDescription] = useState("");
+  const [productPrice, setproductPrice] = useState(0);
+
+  /**The add product METHOD */
+  const addToProductList = () => {
+    Axios.post("http://localhost:3001/insertProduct", {
+      productID: productID,
+      productName: productName,
+      productDescrip: productDescription,
+      productPrice: productPrice,
+    });
+    window.location.reload(false);
+  }
+
+  return(
+    <div>
+      <div>
+        <h3>Add Product Details</h3>
+
+<       label>Product ID </label>
+        <input type="text" onChange={(event) => {
+          setproductID(event.target.value)
+        }} />
+
+        <label>Product Name</label>
+        <input type="text"  onChange={(event) => {
+          setproductName(event.target.value)
+        }} />
+
+        <label>Product Description</label>
+        <input type="text"  onChange={(event) => {
+          setproductDescription(event.target.value)
+        }} />
+
+        <label>Product Price</label>
+        <input type="text"  onChange={(event) => {
+          setproductPrice(event.target.value)
+        }} />
+
+        <button onClick={addToProductList}>Add Product Details</button>
+      </div>
+
     </div>
-  );
+  )
+
 }
 
 export default App;
+
